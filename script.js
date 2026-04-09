194a555e271b1fa74c91546297abfc8a98541a2e
Selim Mohammed-Seguin <91549026+SelimMS@users.noreply.github.com>
2026-04-09 13:14:58 +0100

    Added user input for rock paper scissors Added logic for game and results

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Computer choice for rock paper scissors
 const getComputerChoice = () => {
   random = Math.floor(Math.random() * 3);
   if (random == 0) {
@@ -11,4 +12,48 @@ const getComputerChoice = () => {
   }
 }
 
-console.log(getComputerChoice())
+// Human choice for rock paper scissors
+const getHumanChoice = () => {
+  choice = prompt("Enter 'rock', 'paper' or 'scissors").toLowerCase()
+  return choice
+}
+
+// Initialising Scores
+let humanScore = 0
+let computerScore = 0
+
+
+// Logic for 1 round of game
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log('You Win! Paper beats Rock');
+    humanScore += 1
+  }
+  else if (humanChoice === 'rock' && computerChoice === 'paper') {
+    console.log('You Lose! Paper beats Rock');
+    computerScore += 1
+  }
+  else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    console.log('You Lose! Scissors beats Paper');
+    computerScore += 1
+  }
+  else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    console.log('You Win! Scissors beats Paper');
+    humanScore += 1
+  }
+  else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    console.log('You Win! Rock beats Scissors');
+    humanScore += 1
+  }
+  else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    console.log('You Lose! Rock beats Scissors');
+    computerScore += 1
+  }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+console.log(`You chose ${humanSelection}`)
+console.log(`Computer chose ${computerSelection}`)
+console.log(playRound(humanSelection, computerSelection))
