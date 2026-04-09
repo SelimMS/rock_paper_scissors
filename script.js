528d9d717875ad4026ff8e8efe280fd073385116
Selim Mohammed-Seguin <91549026+SelimMS@users.noreply.github.com>
2026-04-09 12:49:44 +0100

    Added initial index.html and script.js Connected the script file to the index.html file Started the initial code for rock paper scissors with random computer choice

---
+++ script.js
@@ -0,0 +1,14 @@
+const getComputerChoice = () => {
+  random = Math.floor(Math.random() * 3);
+  if (random == 0) {
+    return 'rock'
+  }
+  else if (random == 1) {
+    return 'paper'
+  }
+  else if (random == 2) {
+    return 'scissors'
+  }
+}
+
+console.log(getComputerChoice())
